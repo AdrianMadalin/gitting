4b747e79ca92b658832e13ce61ff0feb06753398
adrian.dinu <adrian.madalin.dinu@gmail.com>
2018-09-02 01:07:44 +0300

    added pages to post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { PostsComponent } from "./posts/posts.component";
+import { PostDetailComponent } from "./posts/post-detail/post-detail.component";
+import { AppRoutingModule } from "./app.routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostsComponent,
+    PostDetailComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
